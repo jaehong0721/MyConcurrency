31c917533e1a1f959aa4c93592e51c190c4579f6
jaehong <newbie302@gmail.com>
2017-03-23 19:18:20 +0900

    큐를 링크드리스트로 변경

--- app/src/main/java/com/rena21c/myconcurrency/MainActivity2.java
+++ app/src/main/java/com/rena21c/myconcurrency/MainActivity2.java
@@ -1,11 +1,10 @@
 package com.rena21c.myconcurrency;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -16,7 +15,7 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Queue<Runnable> queue = new LinkedList<>();
+        final LinkedList<Runnable> queue = new LinkedList<>();
         tvOutput = (TextView) findViewById(R.id.tvOutput);
         appendTextOnUiThread(getClass().getSimpleName() + " >>>>>>>>>>>>");
 
@@ -45,17 +44,17 @@ public class MainActivity2 extends AppCompatActivity {
         });
 
         //동시에 수행됨
-        for(Runnable runnable : queue) {
+        for(int i=0; i<queue.size(); i++) {
+            Runnable runnable = queue.get(i);
             Thread t = new Thread(runnable);
             t.start();
         }
 
-        //순차적으로 실행
         new Thread(new Runnable() {
             @Override
             public void run() {
-                for(Runnable runnable : queue) {
-                    runnable.run();
+                while (!queue.isEmpty()) {
+                    queue.poll().run();
                 }
             }
         }).start();
